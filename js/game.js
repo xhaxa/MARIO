48bdc1c80ade19b33d5357624b5a3714f8938628
Fer Martin <fer@reboot.academy>
2021-03-22 21:57:06 +0000

    clouds movement

--- js/game.js
+++ js/game.js
@@ -3,7 +3,8 @@ const GAME = {
   mario: {
     posX: 100,
     html: document.getElementById('mario')
-  }
+  },
+  clouds: []
 }
 
 const canvas = document.getElementById('canvas')
@@ -12,13 +13,28 @@ function updateMario() {
   GAME.mario.html.style.left = GAME.mario.posX + 'px'
 }
 
+function updateClouds() {
+  GAME.clouds.forEach(function (cloud) {
+    cloud.posX -= 30
+    cloud.html.style.left = `${cloud.posX}px`
+  })
+}
+
 function cloudGeneration() {
   for (var i = 0; i < GAME.numClouds; i++) {
-    const cloud = document.createElement('div')
-    cloud.classList.add('cloud')
-    cloud.style.top = `${ Math.floor(Math.random()*200) + 30 }px`
-    cloud.style.left = `${(i*200) + Math.floor((Math.random() * 100))}px`
-    canvas.appendChild(cloud)
+    let cloud = {
+      posY: Math.floor(Math.random() * 200) + 30,
+      posX: (i * 200) + Math.floor((Math.random() * 100))
+    }
+
+    const cloudHTML = document.createElement('div')
+    cloudHTML.classList.add('cloud')
+    cloudHTML.style.top  = `${ cloud.posY }px`
+    cloudHTML.style.left = `${ cloud.posX }px`
+    canvas.appendChild(cloudHTML)
+
+    cloud.html = cloudHTML;
+    GAME.clouds.push(cloud)
   }
 }
 
@@ -28,8 +44,12 @@ function init() {
 
 document.addEventListener('keydown', function (event) {
   if (event.code === 'ArrowRight') {
-    GAME.mario.posX += 30
     GAME.mario.html.classList.remove('mario-left')
+    if (GAME.mario.posX >= 600) {
+      updateClouds()
+    } else {
+      GAME.mario.posX += 30
+    }
   }
   if (event.code === 'ArrowLeft') {
     GAME.mario.posX -= 30
